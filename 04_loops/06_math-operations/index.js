eb463ec3cdcac15b0166b8731f5d6d4c5f52c5e3
sagvel <saulko.oleksander@gmail.com>
2022-05-10 18:50:51 +0300

    fix

--- 04_loops/06_math-operations/index.js
+++ 04_loops/06_math-operations/index.js
@@ -6,11 +6,17 @@ let result = 0;
 for (let i = start; i <= end; i += 1) {
   if (i % 5 === 0) {
     console.log(i);
-  } else if (i % 2 === 0 && i % 4 === 1) {
+  }
+
+  if (i % 2 === 0 && i % 4 === 1) {
     result += i;
-  } else if (i % 3 === 0) {
+  }
+
+  if (i % 3 === 0) {
     result -= i;
-  } else if (i % 4 === 0) {
+  }
+
+  if (i % 4 === 0) {
     result *= i;
   }
 }
