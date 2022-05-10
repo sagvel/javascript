6b2027bfa2dbe0134da8d543c30d210701d5875d
sagvel <saulko.oleksander@gmail.com>
2022-05-10 19:19:50 +0300

    fix

--- 04_loops/07_find-odds-sum/index.js
+++ 04_loops/07_find-odds-sum/index.js
@@ -4,10 +4,10 @@ for (let i = 0; i <= 1000; i += 1) {
   if (i % 2 === 1) {
     sumOdd += i;
     console.log('Found');
-    if (sumOdd * 5 > 5000) {
-      console.log('Bigger');
-    } else {
-      console.log('Smaller or equal');
-    }
+  }
+  if (i % 2 === 1 && sumOdd * 5 > 5000) {
+    console.log('Bigger');
+  } else if (i % 2 === 1 && sumOdd * 5 < 5000) {
+    console.log('Smaller or equal');
   }
 }
