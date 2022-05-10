98b1c79da87eb242a5a91e6a120cd4c83734d786
sagvel <saulko.oleksander@gmail.com>
2022-05-10 19:14:36 +0300

    solution 4-7

---
+++ 04_loops/07_find-odds-sum/index.js
@@ -0,0 +1,14 @@
+let sumOdd = 0;
+
+for (let i = 0; i <= 1000; i += 1) {
+  if (i % 2 === 1) {
+    sumOdd += i;
+    console.log('Found');
+  }
+
+  if (sumOdd * 5 > 5000) {
+    console.log('Bigger');
+  } else {
+    console.log('Smaller or equal');
+  }
+}
