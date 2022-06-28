53cac335a0c883090ababade0c8663687b1a63e8
sagvel <saulko.oleksander@gmail.com>
2022-06-28 15:14:56 +0300

    add 19.4 solution

---
+++ 19_prototypes/04_user_object/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ');
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
