a78ece8dd22626e110769c997a249e8594eb92d7
sagvel <saulko.oleksander@gmail.com>
2022-06-30 19:27:31 +0300

    add 21.2 solution

---
+++ 21_dom/03_find_many_elements/index.js
@@ -0,0 +1,18 @@
+
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+
+  console.dir(elementsList);
+  return elementsList;
+}
+
+export const getItemsArray = () => {
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+
+  console.dir(elementsArray);
+  return elementsArray;
+}
+
+//test data
+getItemsList();
+getItemsArray();
