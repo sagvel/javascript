63e30205794aafad6165c49df1eb90a6d2f8661c
sagvel <saulko.oleksander@gmail.com>
2022-07-01 10:07:55 +0300

    add 21.5 solution

---
+++ 21_dom/05_update_text_content/index.js
@@ -0,0 +1,12 @@
+// input: string
+// output: undefined
+
+export const setTitle = text => {
+  document.querySelector('.title').textContent = text;
+};
+
+// test data
+
+console.log(document.querySelector('.title').textContent);
+setTitle('Hello! This is test call');
+console.log(document.querySelector('.title').textContent);
