9a30f664d9470ad106470798db93e83aaae12f5b
sagvel <saulko.oleksander@gmail.com>
2022-07-04 15:16:50 +0300

    add 24.2 solution

---
+++ 24_date/02_time_utc/index.js
@@ -0,0 +1,15 @@
+const formatter = new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
+
+// input: date object
+// output: string
+function getGreenwichTime(date) {
+  return formatter.format(date);
+}
+
+// test data
+// console.log(getGreenwichTime(new Date()));
