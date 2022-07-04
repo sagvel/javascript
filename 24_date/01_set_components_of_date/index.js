0975b1c619ef263f4f2b6f6cc070054e0af55bc0
sagvel <saulko.oleksander@gmail.com>
2022-07-04 14:50:34 +0300

    add 24.1 solution

---
+++ 24_date/01_set_components_of_date/index.js
@@ -0,0 +1,14 @@
+// input: Date object, number
+// output: undefined
+
+export function dayOfWeek(date, days) {
+  const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  console.log(weekDay[new Date(dateInFuture).getDay()]);
+}
+
+// test data
+// dayOfWeek(new Date(2022, 6, 4), 5);
+// dayOfWeek(new Date(2021, 6, 4), 5);
