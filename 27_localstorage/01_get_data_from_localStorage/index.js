26d2a39d596c5cfdd54008059e6bf9477811555a
sagvel <saulko.oleksander@gmail.com>
2022-07-04 21:30:10 +0300

    27.1 fixes after tests

--- 27_localstorage/01_get_data_from_localStorage/index.js
+++ 27_localstorage/01_get_data_from_localStorage/index.js
@@ -2,13 +2,16 @@
 // output: object
 
 export function getLocalStorageData() {
-  return (
-    Object.entries(localStorage).reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: value,
-      }),
-      {},
-    ) || {}
-  );
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (error) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  });
 }
