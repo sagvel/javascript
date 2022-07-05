859194aee843fe594c6b117da005db30bf6cbc66
sagvel <saulko.oleksander@gmail.com>
2022-07-05 17:45:50 +0300

    add  28.5 1 version solution

--- 28_functions_advanced/05_shmoment_task/index.js
+++ 28_functions_advanced/05_shmoment_task/index.js
@@ -5,27 +5,75 @@ export const shmoment = initialDate => {
   const getDates = {
     years: date.getFullYear(),
     months: date.getMonth(),
+    days: date.getDate(),
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    milliseconds: date.getMilliseconds(),
   };
 
-  const setDates = {
-    years: 'setFullYear',
-    months: date.setMonth,
-  };
+  // const setDates = {
+  //   years: 'setFullYear',
+  //   months: date.setMonth,
+  // };
 
   const operations = {
     add(type, value) {
-      console.log(setDates[type]);
-      date.setDates[type](getDates[type] + value);
-      // switch (type) {
-      //   case 'years':
-      //     date.setDates[type](getDates[type] + value);
-      //     break;
-      //   case 'months':
-      //     date.setMonth(getDates[type] + value);
-      //     break;
-      //   default:
-      //     break;
-      // }
+      // console.log(setDates[type]);
+      // date.setDates[type](getDates[type] + value);
+      switch (type) {
+        case 'years':
+          date.setFullYear(getDates[type] + value);
+          break;
+        case 'months':
+          date.setMonth(getDates[type] + value);
+          break;
+        case 'days':
+          date.setDate(getDates[type] + value);
+          break;
+        case 'hours':
+          date.setHours(getDates[type] + value);
+          break;
+        case 'minutes':
+          date.setMinutes(getDates[type] + value);
+          break;
+        case 'seconds':
+          date.setSeconds(getDates[type] + value);
+          break;
+        case 'milliseconds':
+          date.setMilliseconds(getDates[type] + value);
+          break;
+        default:
+          break;
+      }
+
+      return this;
+    },
+    subtract(type, value) {
+      switch (type) {
+        case 'years':
+          date.setFullYear(getDates[type] - value);
+          break;
+        case 'months':
+          date.setMonth(getDates[type] - value);
+          break;
+        case 'days':
+          date.setDate(getDates[type] - value);
+          break;
+        case 'hours':
+          date.setHours(getDates[type] - value);
+          break;
+        case 'minutes':
+          date.setMinutes(getDates[type] - value);
+          break;
+        case 'seconds':
+          date.setSeconds(getDates[type] - value);
+          break;
+        case 'milliseconds':
+          date.setMilliseconds(getDates[type] - value);
+          break;
+        default:
+          break;
+      }
 
       return this;
     },
@@ -38,4 +86,10 @@ export const shmoment = initialDate => {
 };
 
 // test data
-console.log(shmoment(new Date(2020, 0, 7, 17, 18, 19, 20)).add('years', 1).result());
+console.log(
+  shmoment(new Date(2020, 0, 7, 17, 18, 19, 20))
+    .add('years', 1)
+    .add('hours', 3)
+    .subtract('minutes', 78)
+    .result(),
+);
