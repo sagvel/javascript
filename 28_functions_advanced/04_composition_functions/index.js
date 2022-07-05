8a8198f3ec15d6f8cb774b7df89ebaa357d19796
sagvel <saulko.oleksander@gmail.com>
2022-07-05 14:24:59 +0300

    add 28.4 solution

---
+++ 28_functions_advanced/04_composition_functions/index.js
@@ -0,0 +1,16 @@
+// input: functions
+// output: number
+
+export const compose =
+  (...funcs) =>
+  value => {
+    return funcs.reduce((acc, func) => func(acc), value);
+  };
+
+// test data
+const add2 = value => value + 2;
+const square = value => value * value;
+const half = value => value / 2;
+const superFunc = compose(add2, square, half);
+
+console.log(superFunc(2)); // 8
