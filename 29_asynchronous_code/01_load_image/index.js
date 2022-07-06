0b18ca3a99c635c49d09f9726150dc704e609427
sagvel <saulko.oleksander@gmail.com>
2022-07-06 15:19:40 +0300

    29.1 comment call of func

--- 29_asynchronous_code/01_load_image/index.js
+++ 29_asynchronous_code/01_load_image/index.js
@@ -37,7 +37,7 @@ const onImageLoaded = (error, imgElem) => {
 };
 
 // examples
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
