6a0d6f2092783744a401cb40886cf1f5e3a9b6ae
sagvel <saulko.oleksander@gmail.com>
2022-07-06 14:28:17 +0300

    add 29.1 start files

---
+++ 29_asynchronous_code/01_load_image/index.js
@@ -0,0 +1,24 @@
+// input: string, callback
+
+const addImage = (imgSrc, callback) => {
+  // put your code here
+};
+
+// callack function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
