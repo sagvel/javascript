469c63680efb32915b2a4818055592506ebbb5eb
sagvel <saulko.oleksander@gmail.com>
2022-07-06 15:16:42 +0300

    add 29.1 solution

--- 29_asynchronous_code/01_load_image/index.js
+++ 29_asynchronous_code/01_load_image/index.js
@@ -1,10 +1,29 @@
 // input: string, callback
+// output: undefined
+export const addImage = (imgSrc, callback) => {
+  const rootElem = document.querySelector('.page');
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
 
-const addImage = (imgSrc, callback) => {
-  // put your code here
+  rootElem.append(imgElem);
+
+  const imgLoadHandler = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  const errorHandler = () => {
+    callback('Image load failed');
+  };
+
+  imgElem.addEventListener('load', imgLoadHandler);
+  imgElem.addEventListener('error', errorHandler);
 };
 
 // callack function
+// input: error || null, DOM element || nothing
+// output: undefined
 const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
@@ -18,7 +37,7 @@ const onImageLoaded = (error, imgElem) => {
 };
 
 // examples
-// addImage(
-//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//   onImageLoaded,
-// );
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
