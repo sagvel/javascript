0bdd932624aa2c419b2bef66b0ce74d64e9252a0
sagvel <saulko.oleksander@gmail.com>
2022-07-07 13:54:59 +0300

    comment test calls

--- 30_promise_intro/05_ promisification/index.js
+++ 30_promise_intro/05_ promisification/index.js
@@ -31,8 +31,8 @@ const addImageV2 = url => {
 //   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
 //   .catch(error => console.log(error)); // ==> 'Image load failed'
 
-addImageV2(
-  'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
-)
-  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch(error => console.log(error)); // ==> 'Image load failed'
+// addImageV2(
+//   'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
+// )
+//   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+//   .catch(error => console.log(error)); // ==> 'Image load failed'
