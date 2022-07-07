a4b849a090bb1c2a377f563161625f344916e839
sagvel <saulko.oleksander@gmail.com>
2022-07-07 15:20:52 +0300

    comment origin code 30.5

--- 30_promise_intro/05_promise_practice/addImage.js
+++ 30_promise_intro/05_promise_practice/addImage.js
@@ -1,21 +1,21 @@
 import { addImageV2 } from './index.js';
 
-const addImage = (url, callback) => {
-  const img = document.createElement('img');
-  img.setAttribute('alt', 'User avatar');
-  img.src = url;
+// const addImage = (url, callback) => {
+//   const img = document.createElement('img');
+//   img.setAttribute('alt', 'User avatar');
+//   img.src = url;
 
-  const pageElem = document.querySelector('.page');
-  pageElem.append(img);
+//   const pageElem = document.querySelector('.page');
+//   pageElem.append(img);
 
-  const onImageLoaded = () => {
-    const { width, height } = img;
-    callback(null, { width, height });
-  };
+//   const onImageLoaded = () => {
+//     const { width, height } = img;
+//     callback(null, { width, height });
+//   };
 
-  const onImageLoadError = () => callback('Image load failed');
+//   const onImageLoadError = () => callback('Image load failed');
 
-  img.addEventListener('load', onImageLoaded);
+//   img.addEventListener('load', onImageLoaded);
 
-  img.addEventListener('error', onImageLoadError);
-};
+//   img.addEventListener('error', onImageLoadError);
+// };
