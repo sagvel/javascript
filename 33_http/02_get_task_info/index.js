0c343be935828ffa1d5813602a5de8bf6064874e
sagvel <saulko.oleksander@gmail.com>
2022-07-09 15:30:39 +0300

    comment call functions

--- 33_http/02_get_task_info/index.js
+++ 33_http/02_get_task_info/index.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-export function getTasksList(url = '') {
+export function getTasksList() {
   return fetch(baseUrl).then(response => response.json());
 }
 
@@ -10,10 +10,10 @@ export function getTaskById(taskId) {
 }
 
 // examples
-getTasksList().then(tasksList => {
-  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
+// getTasksList().then(tasksList => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
 
-getTaskById('1').then(taskData => {
-  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
+// getTaskById('1').then(taskData => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
