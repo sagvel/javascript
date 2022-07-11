59ab79b8ef1bb61a07db3a2a61c32ef69002bde8
sagvel <saulko.oleksander@gmail.com>
2022-07-11 19:24:45 +0300

    add 34.2 solution

---
+++ 34_http_practice/02_crud/index.js
@@ -0,0 +1,68 @@
+const BASE_URL = 'https://62cbcd06a0800529309ee109.mockapi.io/api/v1/users';
+
+function getUsersList() {
+  return fetch(BASE_URL).then(res => res.json());
+}
+
+export function getUserById(userId) {
+  return fetch(`${BASE_URL}/${userId}`).then(res => res.json());
+}
+
+export function createUser(userData) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+export function deleteUser(userId) {
+  return fetch(`${BASE_URL}/${userId}`, { method: 'DELETE' });
+}
+
+export function updateUser(userId, userData) {
+  return fetch(`${BASE_URL}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+// examples
+// getUsersList().then(users => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then(userData => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('2').then(() => {
+//   console.log('User updated');
+// });
